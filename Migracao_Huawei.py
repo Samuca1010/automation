b1ff70e28c9bd361ea342f86eada26cea56637fd
Samuca1010 <120064155+Samuca1010@users.noreply.github.com>
2024-07-10 17:15:43 -0300

    Update Migracao_Huawei.py

--- Migracao_Huawei.py
+++ Migracao_Huawei.py
@@ -10,25 +10,34 @@ PON = "0"
 slotCard = "0/1"
 vlanMGMT = (240, 21)
 vlanPPPOE = (160, 20)
-vlanMGMT_VOIP = (240, 21)
-vlanPPPOE_VOIP = (161, 22)
+vlanMGMT_VOIP = (240, 203)
+vlanPPPOE_VOIP = (160, 202)
+vlanVOIP = (161, 204)
 lineProfileID = "5"
 srvProfileID = "1"
+lineProfileID_VOIP = "81"
 
 # Funções para leitura e filtragem dos seriais
-def read_and_filter_serials(file_path):
+def filter_Serials(file_path):
     try:
         with open(file_path, 'r') as file:
             lines = file.readlines()
         print(f"Total de linhas lidas: {len(lines)}")
-        # Adaptando para extrair o serial da linha que começa com "onu"
+        
         filtered_lines = []
         for line in lines:
             line = line.strip().upper()
             if line.startswith("ONU"):
                 parts = line.split()
                 if len(parts) > 1 and (parts[1].startswith("HWTC")):
-                    filtered_lines.append(parts[1])
+                    serial = parts[1]
+                    # Procurar pelo flow-profile
+                    flow_profile_match = re.search(r'FLOW-PROFILE\s+(\S+)', line, re.IGNORECASE)
+                    if flow_profile_match:
+                        flow_profile = flow_profile_match.group(1).lower()
+                    else:
+                        flow_profile = None
+                    filtered_lines.append((serial, flow_profile))
         print(f"Total de seriais filtrados: {len(filtered_lines)}")
         return filtered_lines
     except FileNotFoundError:
@@ -58,7 +67,7 @@ def analyzesBuffer(shell, command):
     return recv_data, stderr_data
 
 try:
-    print("Conectando-se ao equipamento")
+    print("Conectando-se ao equipamento...")
     with paramiko.SSHClient() as ssh:
         ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         ssh.connect(host, username=user, password=password)
@@ -66,25 +75,34 @@ try:
 
         shell = ssh.invoke_shell()
 
-        file_path = r"C:\Users\Telecom\Downloads\OLT-067-TESTE.txt" 
-        serials = read_and_filter_serials(file_path)
+        file_path = r"C:\Users\Telecom\Downloads\OLT-64-PERFIS-HUAWEI.txt"
+        serials_and_profiles = filter_Serials(file_path)
         
-        if not serials:
+        if not serials_and_profiles:
             print("Nenhum serial válido encontrado no arquivo.")
         else:
+            # Flag para controlar quando executar o comando "enable"
+            enable_executed = False  
 
-            enable_executed = False  # Flag para controlar quando executar "enable"
+            for serial, flow_profile in serials_and_profiles:
+                print(f"Processando serial: {serial} com flow-profile: {flow_profile}")
 
-            for serial in serials:
-                print(f"Processando serial: {serial}")
+                # Definir o lineProfileID de acordo se for router ou router voip
+                if flow_profile in ["router_1g_huawei", "router_1g_huawei_default_voip", "router_1g_huawei_telefonia"]:
+                    lineProfileID_current = lineProfileID_VOIP
+                elif flow_profile in ["router_1g_huawei_dados", "router_1g_huawei_default"]:
+                    lineProfileID_current = lineProfileID
+                else:
+                    print(f"Perfil incorreto para o serial: {serial} ont pulando")
+                    continue
 
-                # Construção do comando add_ont_command
+                # Montando o comando para add ont
                 if not enable_executed:
                     add_ont_command = (
                         "enable\n"
                         "config\n"
                         f"interface gpon {slotCard}\n"
-                        f"ont add {PON} sn-auth {serial} omci ont-lineprofile-id {lineProfileID} ont-srvprofile-id {srvProfileID} desc 'milene_cristina_toloto_sattis_2'\n\n"
+                        f"ont add {PON} sn-auth {serial} omci ont-lineprofile-id {lineProfileID_current} ont-srvprofile-id {srvProfileID} desc 'milene_cristina_toloto_sattis_2'\n\n"
                         "quit\n"
                         "quit\n"
                     )
@@ -93,7 +111,7 @@ try:
                     add_ont_command = (
                         "config\n"
                         f"interface gpon {slotCard}\n"
-                        f"ont add {PON} sn-auth {serial} omci ont-lineprofile-id {lineProfileID} ont-srvprofile-id {srvProfileID} desc 'milene_cristina_toloto_sattis_2'\n\n"
+                        f"ont add {PON} sn-auth {serial} omci ont-lineprofile-id {lineProfileID_current} ont-srvprofile-id {srvProfileID} desc 'milene_cristina_toloto_sattis_2'\n\n"
                         "quit\n"
                         "quit\n"
                     )
@@ -114,16 +132,23 @@ try:
                     ont_id = match.group(1)
                     print(f"ID da ONT: {ont_id}")
 
-                    # Comandos para configurar os service-ports
-                    service_port_commands = [
-                        "config\n",
-                        f"service-port vlan {vlanMGMT[0]} gpon {slotCard}/{PON} ont {ont_id} gemport {vlanMGMT[1]} multi-service user-vlan {vlanMGMT[0]} tag-transform translate\n\n",
-                        f"service-port vlan {vlanPPPOE[0]} gpon {slotCard}/{PON} ont {ont_id} gemport {vlanPPPOE[1]} multi-service user-vlan {vlanPPPOE[0]} tag-transform translate\n\nquit\n"
-                    ]
-                    
-
+                    # Montando comandos para configurar os services
+                    if flow_profile in ["router_1g_huawei", "router_1g_huawei_default_voip", "router_1g_huawei_telefonia"]:
+                        service_port_commands = [
+                            "config\n",
+                            f"service-port vlan {vlanMGMT_VOIP[0]} gpon {slotCard}/{PON} ont {ont_id} gemport {vlanMGMT_VOIP[1]} multi-service user-vlan {vlanMGMT_VOIP[0]} tag-transform translate\n\n",
+                            f"service-port vlan {vlanPPPOE_VOIP[0]} gpon {slotCard}/{PON} ont {ont_id} gemport {vlanPPPOE_VOIP[1]} multi-service user-vlan {vlanPPPOE_VOIP[0]} tag-transform translate\n\n",
+                            f"service-port vlan {vlanVOIP[0]} gpon {slotCard}/{PON} ont {ont_id} gemport {vlanVOIP[1]} multi-service user-vlan {vlanVOIP[0]} tag-transform translate\n\nquit\n"
+                        ]
+                    else:
+                        service_port_commands = [
+                            "config\n",
+                            f"service-port vlan {vlanMGMT[0]} gpon {slotCard}/{PON} ont {ont_id} gemport {vlanMGMT[1]} multi-service user-vlan {vlanMGMT[0]} tag-transform translate\n\n",
+                            f"service-port vlan {vlanPPPOE[0]} gpon {slotCard}/{PON} ont {ont_id} gemport {vlanPPPOE[1]} multi-service user-vlan {vlanPPPOE[0]} tag-transform translate\n\nquit\n"
+                        ]
+                    #
                     for command in service_port_commands:
-                        print("Cheguei aqui")
+                        print("Executando comando:", command)
                         recv_data, stderr_data = analyzesBuffer(shell, command)
                         print(f"Saída do comando 'service_port_command':")
                         print(recv_data)
